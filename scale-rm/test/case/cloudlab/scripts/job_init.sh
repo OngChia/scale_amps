b6d22273f9354891eeb4cf42823f94be2f521ccd
Chia Rui Ong <chiarui.ong@c2sm.ethz.ch>
2025-05-27 12:36:57 +0200

    add cloudlab testcase

---
+++ scale-rm/test/case/cloudlab/scripts/job_init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --ntasks=10
+#SBATCH --ntasks-per-node=10
+#SBATCH --time=1:00:00
+#SBATCH --job-name="init_sheba"
+###SBATCH --mem-per-cpu=1024
+###SBATCH --output="LOG"
+#SBATCH --error="ERROR_LOG_INIT"
+###SBATCH --open-mode=truncate
+
+module load stack/2024-06
+module load openmpi/4.1.6
+module load netcdf-c
+module load netcdf-fortran
+module load hdf5
+module load zlib/1.3-mktm5vz
+
+export OMP_STACKSIZE=16M
+
+mpirun -n 10 ./scale-rm_init init.conf
